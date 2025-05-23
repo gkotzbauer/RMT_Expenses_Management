0ad2e7d95957be018335f3ffb8f7e6b7e8be75a1
Kotz <greg.kotzbauer@gmail.com>
2025-05-23 10:53:28 -0400

    Update clinic_dash_app.py

--- clinic_dash_app.py
+++ clinic_dash_app.py
@@ -216,4 +216,4 @@ def update_output(contents, filename, download_clicks):
     )
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
